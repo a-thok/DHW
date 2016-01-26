92dec824e9c7e669b48c11e0f9c08718f32a64f4
Victor <a-thok@outlook.com>
2016-01-26 09:43:11 +0800

    chore: 开发环境设置

---
+++ usercenter/webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: {
+    usercenter: './src/js/entry.js'
+  },
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'js/[name].js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.css']
+  },
+  externals: [
+    {
+      'jquery': 'window.jQuery',
+      'angular': 'window.angular'
+    }
+  ],
+  module: {
+    //加载器配置
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!cssnext')
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel?presets=es2015',
+        exclude: '/node_modules/'
+      },
+      // {
+      //   test: /\.(woff|eot|ttf)$/i,
+      //   loader: 'url?limit=10000&name=fonts/[hash:8].[name].[ext]'
+      // },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'file?name=img/[name].[ext]?[hash]'
+      }
+    ]
+  },
+  plugins: [
+    // new webpack.optimize.CommonsChunkPlugin('js/common.js'),
+    new ExtractTextPlugin('css/[name].css')
+  ],
+  cssnext: {
+    compress: true,
+    features: {
+      // rem: false,
+      // pseudoElements: false,
+      // colorRgba: false
+    }
+  }
+};
