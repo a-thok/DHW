5f83cf9cd2ac4fd8d95f19e1cdad469a235630fb
He Wei <hw961919531@outlook.com>
2016-01-30 09:27:37 +0800

    refactor: 列表页面

--- usercenter/src/js/directives/formComponents.js
+++ usercenter/src/js/directives/formComponents.js
@@ -34,6 +34,52 @@ app
       scope: true
     }
   })
+  .directive('listHeader', function() {
+    return {
+      template: function(elem, attrs) {
+        return `
+          <thead class="listThead">
+            <tr>
+              <th ng-repeat="${attrs.repeat}">{{item}}</th>
+            </tr>
+          </thead>
+        `
+      },
+      replace: true,
+      scope: true
+    }
+  })
+  .directive('listColGroup', function() {
+    return {
+      template: function(elem, attrs) {
+        return `
+          <colgroup>
+            <col ng-repeat="${attrs.repeat}" width="auto">
+          </colgroup>
+        `
+      },
+      replace: true,
+      scope: true
+    }
+  })
+  .directive('listContent', function() {
+    return {
+      template: function(elem, attrs) {
+        return `
+          <tr ng-repeat="${attrs.repeat}">
+            <td>{{items.position}}</td>
+            <td>{{items.money}}</td>
+            <td>{{items.gznx}}</td>
+            <td>{{items.xl}}</td>
+            <td>{{items.city}}</td>
+            <td><a href="#">查看详情</a></td>
+          </tr>
+        `
+      },
+      replace: true,
+      scope: true
+    }
+  })
   .directive('inputText', function() {
     return {
       template: function(elem, attrs) {
