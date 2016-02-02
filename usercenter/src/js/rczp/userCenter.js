9ffd9ef7db40703f45fa6c11ab9e2f8958501e39
jixu <724921588@qq.com>
2016-02-02 18:14:35 +0800

    style:  清除merge产生的无用字符

--- usercenter/src/js/rczp/userCenter.js
+++ usercenter/src/js/rczp/userCenter.js
@@ -10,11 +10,8 @@ import listComponents  from '../directives/listComponents.js' //已投简历引�
 import MainCtrl from './controllers/MainCtrl.js';
 import ZplbCtrl from './controllers/ZplbCtrl.js';
 import FbzpCtrl from './controllers/FbzpCtrl.js';
-<<<<<<< HEAD
-import YtjlCtrl from './controllers/YtjlCtrl.js'
-=======
-import JlbjCtrl from './controllers/JlbjCtrl.js'; // 卢铭怀 引入控制器函数
->>>>>>> f7aaed177a45f453c075aa79034b00f34b365ff4
+// import YtjlCtrl from './controllers/YtjlCtrl.js'
+// import JlbjCtrl from './controllers/JlbjCtrl.js'; // 卢铭怀 引入控制器函数
 
 let app = angular.module('userCenter', ['ui.router', 'formComponents', 'listComponents','ui.bootstrap.pagination']);
 app
@@ -38,7 +35,7 @@ app
       .state('jlbj', {
         url: '/jlbj',
         templateUrl: '/partials/rczp/partial-jlbj.html',
-        controller: 'JlbjCtrl as jlbjVm' // 卢铭怀 添加路由子控制器
+        // controller: 'JlbjCtrl as jlbjVm' // 卢铭怀 添加路由子控制器
       })
       .state('jlyl', {
         url: '/jlyl',
@@ -47,7 +44,7 @@ app
       .state('ytjl', {
         url: '/ytjl',
         templateUrl: '/partials/rczp/partial-ytjl.html',
-        controller:  'YtjlCtrl as ytjlVm'                  //已投简历的控制器
+        // controller:  'YtjlCtrl as ytjlVm'                  //已投简历的控制器
       })
       .state('zwsc', {
         url: '/zwsc',
@@ -57,10 +54,7 @@ app
   .controller('MainCtrl', [MainCtrl])
   .controller('zplb', [ZplbCtrl])
   .controller('FbzpCtrl', ['$http', FbzpCtrl])
-<<<<<<< HEAD
-  .controller('YtjlCtrl',['$http',YtjlCtrl]);
-=======
-  .controller('JlbjCtrl', ['$scope', '$http', '$location', JlbjCtrl]); // 卢铭怀 定义控制器
+  // .controller('YtjlCtrl',['$http',YtjlCtrl])
+  // .controller('JlbjCtrl', ['$scope', '$http', '$location', JlbjCtrl]); // 卢铭怀 定义控制器
 
->>>>>>> f7aaed177a45f453c075aa79034b00f34b365ff4
 export default app;
