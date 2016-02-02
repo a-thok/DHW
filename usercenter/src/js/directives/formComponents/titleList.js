f4386cf4907ba08a3e13741523aca8289dcdd521
Victor <a-thok@outlook.com>
2016-02-02 17:15:06 +0800

    refactor: 列表页标签切换指令优化

--- usercenter/src/js/directives/formComponents/titleList.js
+++ usercenter/src/js/directives/formComponents/titleList.js
@@ -6,11 +6,21 @@ export default function titleList() {
       return `
         <ul class="titleTabs clearfix">
           <li class="titleTab" 
-            ng-class="{active:whichTab=='{{item}}'}"
-            ng-click="whichTab='{{item}}'"
+            ng-class="{ active: vm.isActive($index) }"
+            ng-click="vm.tabChanged($index)"
             ng-repeat="${attrs.repeat}">{{item}}</li>
         </ul>
       `
-    }
+    },
+    controller: function() {
+      let vm = this;
+      // 当前选中标签，默认第一个
+      vm.currentTab = 0;
+      // 用$index判断每个标签是不是当前标签
+      vm.isActive = index => index === vm.currentTab;
+      // 点击标签时，根据当前标签的$index改变currentTab的值
+      vm.tabChanged = index => vm.currentTab = index;
+    },
+    controllerAs: 'vm'
   }
 }
