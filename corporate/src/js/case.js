173813556c5d81f7ec7cbc9e269f4e63b4f5948b
He Wei <hw961919531@outlook.com>
2016-02-03 13:42:07 +0800

    refactor: 企业站案例展示

---
+++ corporate/src/js/case.js
@@ -0,0 +1,12 @@
+// 样式表
+import '../css/cmpy.css';
+
+// 图片
+import '../img/anzs_03.png';
+import '../img/anzs_05.png';
+import '../img/anzs_07.png';
+import '../img/anzs_09.png';
+import '../img/iconfont-icon05.png';
+
+// JS
+
