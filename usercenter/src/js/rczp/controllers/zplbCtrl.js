1cad1fc99f3ace62c7c9916e2d9fd92bf95cf7f4
He Wei <hw961919531@outlook.com>
2016-02-17 14:00:06 +0800

    fix: 招聘列表头部

--- usercenter/src/js/rczp/controllers/zplbCtrl.js
+++ usercenter/src/js/rczp/controllers/zplbCtrl.js
@@ -1,6 +1,6 @@
 export default function ZplbCtrl(){
   var vm = this;
-  vm.listTabs = [{'url':'.published',
+  vm.listtitle = [{'url':'.published',
                     'name' : '已发布的招聘'},     
                    {'url':'.offline',
                     'name' : '已下线的招聘'
