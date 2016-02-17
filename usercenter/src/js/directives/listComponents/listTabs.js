8507f9d2242e8e50a2a2b1a7d391b0efc2de145b
He Wei <hw961919531@outlook.com>
2016-02-17 13:49:12 +0800

    fix: 列表头部的修改

--- usercenter/src/js/directives/listComponents/listTabs.js
+++ usercenter/src/js/directives/listComponents/listTabs.js
@@ -7,22 +7,33 @@ export default function listTabs() {
         <ul class="titleTabs clearfix">
           <a  ng-repeat="${attrs.repeat}" ui-sref="{{item.url}}">
             <li class="titleTab" 
+            data-url="{{item}}"
             ng-class="{ active: vm.isActive($index) }"
-            ng-click="vm.tabChanged($index)"
+            ng-click="vm.tabChanged($index, $event)"
              >{{item.name}}</li>
           </a>
         </ul>
       `
     },
-    controller: function() {
+    controller: ['$location', '$attrs', '$scope', function($location, $attrs, $scope) {
       let vm = this;
       // 当前选中标签，默认第一个
-      vm.currentTab = 0;
+      let currentTab = 0;
+      let arr = $scope.$parent[$attrs.vm].listtitle
+      let path = $location.path().lastIndexOf('/')
+      let url = '.' + $location.path().substring(path + 1);
+      arr.forEach(function(currentValue, index) {
+        if(url === currentValue.url) {
+          currentTab = index
+        }
+      })
       // 用$index判断每个标签是不是当前标签
-      vm.isActive = index => index === vm.currentTab;
+      vm.isActive = index => index === currentTab;
       // 点击标签时，根据当前标签的$index改变currentTab的值
-      vm.tabChanged = index => vm.currentTab = index; 
-    },
+      vm.tabChanged = (index, event) => {
+        currentTab = index;
+      }
+    }],
     controllerAs: 'vm'
   }
 }
