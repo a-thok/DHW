7ef69f1faf2fad2b61edc41d5e38cd1334b416d3
zhuangdize <974722532@QQ .COM>
2016-02-17 17:47:13 +0800

    refactor:重构

--- usercenter/src/js/rczp/controllers/zplbCtrl.js
+++ usercenter/src/js/rczp/controllers/zplbCtrl.js
@@ -1,6 +1,6 @@
 export default function ZplbCtrl(){
   var vm = this;
-  vm.listtitle = [{'url':'.published',
+  vm.listTabs = [{'url':'.published',
                     'name' : '已发布的招聘'},     
                    {'url':'.offline',
                     'name' : '已下线的招聘'
