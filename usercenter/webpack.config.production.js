3f515837f0680d57eea019361a0027598dc86fc1
Victor <a-thok@outlook.com>
2016-02-19 10:46:53 +0800

    chore: 不再打包angular

--- usercenter/webpack.config.production.js
+++ usercenter/webpack.config.production.js
@@ -3,7 +3,6 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: {
-    angular: ['angular/angular.js', 'angular-ui-router', 'angular-animate'],
     'uc.rczp': [__dirname + '/src/js/rczp.js']
   },
   output: {
@@ -40,11 +39,7 @@ module.exports = {
       {
         test: /\.(png|jpg|gif)$/,
         loader: 'url?limit=8192&name=img/uc/[name].[ext]?[hash]'
-      },
-      {
-        test: require.resolve('angular'),
-        loader: 'expose?angular'
-      },
+      }
     ]
   },
   plugins: [
