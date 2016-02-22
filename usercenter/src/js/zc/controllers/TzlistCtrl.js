11bf077cc7af2c10e143fdafae7c7beb4139de36
jixu <724921588@qq.com>
2016-02-22 13:44:31 +0800

    refactor: 众筹关注支持列表重构

--- usercenter/src/js/zc/controllers/TzlistCtrl.js
+++ usercenter/src/js/zc/controllers/TzlistCtrl.js
@@ -1,7 +1,7 @@
 export default function TzlistCtrl() {
   var vm = this;
   vm.list=[
-    {name:'项目标题', key:'', width:'21%', link:'//hr.dreamhiway.com/cpzcDetail/', linkey:'fpid'},
+    {name:'项目标题', key:'', width:'21%'},
     {name:'项目类型', key:'', width:'15%'},
     {name:'项目状态', key:'', width:'15%'},
     {name:'筹资进度', key:'', width:'16%'},
