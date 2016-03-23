d75fdd7bd5ecbe81b846e2ce70bfc9bc8c5fb0de
He Wei <hw961919531@outlook.com>
2016-03-23 11:15:44 +0800

    商标超市上传测试

---
+++ usercenter/src/js/sbcs/controllers/SplbControllers/JyzCtrl.js
@@ -0,0 +1,12 @@
+export default function JyzCtrl() {
+  let vm = this;
+  vm.list = [
+    { name: '商标图片', key: 'img', width: '15%', img: true },
+    { name: '商标名称', key: 'projectName', width: '10%' },
+    { name: '使用范围', key: 'type', width: '20%' },
+    { name: '状态', key: 'state', width: '10%' },
+    { name: '购买价格', key: 'price', width: '15%' },
+    { name: '转让价格', key: 'sellprice', width: '10%' },
+    { name: '交易形式', key: 'tradetypeName', width: '10%' },
+  ]
+}
