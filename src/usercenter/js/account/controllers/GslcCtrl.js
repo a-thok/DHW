4f48ea3e48cf0c52c4e20103a70993ab41d3612f
zhuangdize <974722532@QQ .COM>
2016-04-12 10:54:19 +0800

    公司历程删除功能

--- src/usercenter/js/account/controllers/GslcCtrl.js
+++ src/usercenter/js/account/controllers/GslcCtrl.js
@@ -1,6 +1,7 @@
 export default function Gslc($http) {
   let vm = this;
   vm.data = {};
+  vm.dtlist = [];
   $http.post('/CompanyHomeEdit/CompanyDevelopmentDetail').success((d) => {
     vm.dtlist = d.result;
   });
@@ -12,6 +13,18 @@ export default function Gslc($http) {
         location.reload();
       } else {
         alert('网络原因，提交失败');
+        location.reload();
+      }
+    });
+  };
+  // 删除历程
+  vm.del = (index, id) => {
+    $http.post('/CompanyHomeEdit/CompanyDevelopmentDel', { id: id }).success((d) => {
+      if (d.success) {
+        alert('删除成功');
+        location.reload();
+      } else {
+        alert('网络原因，删除失败');
         location.reload();
       }
     });
