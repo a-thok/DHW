469b691159d196bb52aa4620e3632a3a047dc686
He Wei <hw961919531@outlook.com>
2016-04-13 16:20:11 +0800

    相册类型字段的添加和欢迎页细节的修改

--- src/usercenter/js/account/controllers/AlbumCtrl.js
+++ src/usercenter/js/account/controllers/AlbumCtrl.js
@@ -3,6 +3,7 @@ export default function AlbumCtrl($http) {
   let vm = this;
   vm.data = {};
   vm.activity = ['创意设计比赛'];
+  vm.type = ['科技发明制作类', '科学设想方案类', '平面设计类', '日常用品创意类', '服装设计类'];
   vm.submit = () => {
     if ($('#negotiable_yes').is(':checked')) {
       vm.data.see = 0;
