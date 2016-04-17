890f7e17fac3047a82cbd8c9f846fcda7d772ec6
He Wei <hw961919531@outlook.com>
2016-04-17 14:07:36 +0800

    欢迎页修改样式,商标超市列表页的修改

--- src/usercenter/js/sbcs/controllers/SplbControllers/JyzCtrl.js
+++ src/usercenter/js/sbcs/controllers/SplbControllers/JyzCtrl.js
@@ -3,7 +3,7 @@ export default function JyzCtrl() {
   vm.list = [
     { name: '商标图片', key: 'img', width: '15%', img: true },
     { name: '商标名称', key: 'projectName', width: '10%' },
-    { name: '使用范围', key: 'type', width: '20%' },
+    { name: '使用范围', key: 'services', width: '20%' },
     { name: '状态', key: 'state', width: '10%' },
     { name: '购买价格', key: 'price', width: '15%' },
     { name: '转让价格', key: 'sellprice', width: '10%' },
