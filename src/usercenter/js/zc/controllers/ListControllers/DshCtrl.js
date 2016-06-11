f82f77a9904826ba528a1fe8778b7dccb5b5e7dc
cjq <hitearth@163.com>
2016-06-11 10:24:18 +0800

    url bug 更改

--- src/usercenter/js/zc/controllers/ListControllers/DshCtrl.js
+++ src/usercenter/js/zc/controllers/ListControllers/DshCtrl.js
@@ -2,7 +2,7 @@ export default function dshCtrl() {
   var vm = this;
   vm.list = [
     { name: '项目图片', key: 'frontpic', width: '15%', img: true },
-    { name: '项目标题', key: 'projectName', width: '20%', link: true, linkkey: 'rowId' },
+    { name: '项目标题', key: 'projectName', width: '20%', link2: true, linkkey: 'url' },
     { name: '回报内容', key: 'hbcontent', width: '20%' },
     { name: '数量', key: 'hbcount', width: '10%' },
     { name: '状态', key: 'status', width: '15%' },
--- src/usercenter/js/zc/controllers/sellerControllers/s_YshCtrl.js
+++ src/usercenter/js/zc/controllers/sellerControllers/s_YshCtrl.js
@@ -1,7 +1,7 @@
 export default function s_YshCtrl($scope) {
   var vm = this;
   vm.list = [
-    { name: '项目标题', key: 'projectname', width: '20%', link: true, linkkey: 'rowId' },
+    { name: '项目标题', key: 'projectname', width: '20%', link2: true, linkkey: 'url' },
     { name: '收件人名称', key: 'receiveName', width: '15%' },
     { name: '回报标题', key: 'hbtitle', width: '15%' },
     { name: '订单编号', key: 'number', width: '20%' },
